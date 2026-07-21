342f35e59f7fcfffb5a183ce6f9cf73473dd1cfe
sukmawijaya160208-a11y <sukmawijaya160208@gmail.com>
2026-07-21 10:40:26 +0700

    fix: chown storage & database at startup for PHP-FPM www-data

--- backend/startup.sh
+++ backend/startup.sh
@@ -3,6 +3,10 @@ set -e
 
 cd /app
 
+chown -R www-data:www-data storage database
+touch storage/logs/laravel.log
+chown www-data:www-data storage/logs/laravel.log
+
 php artisan config:clear
 php artisan route:clear
 php artisan config:cache
